c619ec8df746a63cb5e710eb307b01ebad30dac9
Julia Ilasova <1julka1il@gmail.com>
2024-05-29 14:29:29 +0200

    upon retaking the image in add plant screen, the new image now gets displayed correctly

--- lib/repositories/camera_repository.dart
+++ lib/repositories/camera_repository.dart
@@ -1,16 +1,15 @@
 import 'package:image_picker/image_picker.dart';
 
-// TODO: check web compatibility
 class PictureRepository {
   Future<XFile?> pickImageFromGallery() async {
-    final ImagePicker _picker = ImagePicker();
-    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
     return image;
   }
 
   Future<XFile?> getImageFromCamera(CameraDevice? preferredCamera) async {
-    final ImagePicker _picker = ImagePicker();
-    final XFile? image = await _picker.pickImage(
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(
       source: ImageSource.camera,
       preferredCameraDevice: preferredCamera ?? CameraDevice.rear,
     );
