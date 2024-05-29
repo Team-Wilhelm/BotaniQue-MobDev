dd131a86eeeb517cce7ee587061be1fa22f4e08d
mariaruth1 <mariaruth1@users.noreply.github.com>
2024-05-29 14:03:12 +0000

    Deploying to gh-pages from @ Team-Wilhelm/BotaniQue-MobDev@7bf12f303ee208c63c6abe6b19fffd6e913e66f0 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1650768147"
+    serviceWorkerVersion: "3526412786"
   }
 });
